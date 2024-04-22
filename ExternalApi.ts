8e82400efdba4c761205ff623fa18824d63dbd34
Pedro Henrique Gomes <130695589+Pedrogom7@users.noreply.github.com>
2024-04-22 13:12:47 -0300

    atualização para busca no servidor da marvel

--- ExternalApi.ts
+++ ExternalApi.ts
@@ -1,15 +1,35 @@
-import fetch from 'node-fetch';
+import fetch, { Response } from 'node-fetch';
+import MarvelResponse from './Interfaces/marvelResponse.interface';
 
 class ExternalApiService {
-  public static async getMarvelData(url: string) {
+  public static async getMarvelData(url: string): Promise<any[]> {
     try {
-      const response = await fetch(url);
-      const data = await response.json();
-      //return data.data.results; // Estou assumindo que a estrutura de resposta da Marvel API é similar a isso
+      const response: Response = await fetch(url);
+      const responseData = await response.json();
+
+      if (ExternalApiService.isMarvelResponse(responseData)) {
+        const data: MarvelResponse = responseData;
+        return data.data.results;
+      } else {
+        throw new Error('Resposta da Marvel API com estrutura inválida');
+      }
     } catch (error) {
       console.error('Erro ao buscar dados da Marvel API:', error);
       throw error;
     }
+  }
+
+  private static isMarvelResponse(data: any): data is MarvelResponse {
+    return (
+      'code' in data &&
+      'status' in data &&
+      'data' in data &&
+      'offset' in data.data &&
+      'limit' in data.data &&
+      'total' in data.data &&
+      'count' in data.data &&
+      'results' in data.data
+    );
   }
 }
 
