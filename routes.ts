e448ee1ad4d7b1aee4e7bc71cb363ef37143b07a
Pedro Henrique Gomes <130695589+Pedrogom7@users.noreply.github.com>
2024-04-22 13:15:17 -0300

    criação das rotas

--- routes.ts
+++ routes.ts
@@ -1,11 +1,26 @@
-import { Router } from "express"
-import personagemController from "./src/Controller/personagem.controller"
+import { Router } from "express";
+import personagemController from "./src/Controller/personagem.controller";
 import quadrinhoController from "./src/Controller/quadrinho.controller";
 import criadorController from "./src/Controller/criador.controller";
 
-const routes = Router()
-routes.get('personagem',personagemController.create);
-routes.get('quadrinho',quadrinhoController.create);
-routes.get('criador',criadorController.create)
+const routes = Router();
 
-export { routes }
+// Rotas para Personagem
+routes.get('/personagem', personagemController.list);
+routes.post('/personagem', personagemController.create);
+routes.put('/personagem/:id', personagemController.update);
+routes.delete('/personagem/:id', personagemController.delete);
+
+// Rotas para Quadrinho
+routes.get('/quadrinhos', quadrinhoController.list);
+routes.post('/quadrinhos', quadrinhoController.create);
+routes.put('/quadrinhos/:id', quadrinhoController.update);
+routes.delete('/quadrinhos/:id', quadrinhoController.delete);
+
+// Rotas para Criador
+routes.get('/criadores', criadorController.list);
+routes.post('/criadores', criadorController.create);
+routes.put('/criadores/:id', criadorController.update);
+routes.delete('/criadores/:id', criadorController.delete);
+
+export { routes };
